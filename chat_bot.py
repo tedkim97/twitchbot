6bb8c24d7fa46312acdd8c12cd884a94a30dcc02
Ted Kim <tedkim97@gmail.com>
2018-01-08 19:05:58 -0500

    updated chat_bot

--- chat_bot.py
+++ chat_bot.py
@@ -1,48 +1,60 @@
 import string
 import socket
 import re
-import pyautogui
-from keys import*
-from pynput import mouse as m 
-from pynput import keyboard as kb
+#import pyautogui
+#from keys import*
+#from pynput import mouse as m 
+#from pynput import keyboard as kb
 
 class tc_bot(object): 
-	"""the chat bot is created as an object because I want to have the option 
+	"""the chat bot is created as an object to give the flexibility
 	of running multiple bots at once given different parameters"""
 
 	def __init__(self,chan,iden,oauth,host="irc.chat.twitch.tv",port=6667): 
-		HOST= host
-		PORT= port
-		ID= iden
-		PASS= pw
-		readbuffer = ""
-		s = openSocket()
+		self.CHANNEL = chan
+		self.ID= iden
+		self.PASS= oauth
+		self.HOST= host
+		self.PORT= port		
+		self.readbuffer = ""
+		self.socket = self.openSocket() 
+		self.joinRoom()
 
-	def encoded_send(self,socket,msg:str):
-		socket.send(msg.encode("utf-8"))
-
-	def joinRoom(self, socket):
+	def joinRoom(self):
 		Loading = True 
 		while Loading: 
-			self.readbuffer= self.readbuffer + (s.recv(1024)).decode("utf-8")
-			temp= readbuffer.split("\n")
-			readbugger= temp.pop()
+			self.readbuffer= self.readbuffer + (self.socket.recv(1024)).decode("utf-8")
+			temp= self.readbuffer.split("\n")
+			self.readbuffer= temp.pop()
 
 			for line in temp: 
 				print(line)
-				Loading = loadgincComplete(line)
-		sendMessage(s, "joined chat")
+				Loading = self.loadingComplete(line)
+		self.sendMessage("joined chat")
 
 	def loadingComplete(self, line): 
 		if("End of /NAMES list" in line): 
 			return False 
 		return True 
 
-
 	def openSocket(self):
 		s = socket.socket()
-		s.connect((HOST, PORT))
+		s.connect((self.HOST, self.PORT))
+		encoded_send(s, "PASS " + self.PASS + "\r\n")
+		encoded_send(s, "NICK " + self.ID + "\r\n")
+		encoded_send(s, "JOIN #" + self.CHANNEL + "\r\n")
+		return s
 
+	def sendMessage(self, message): 
+		messageTemp = "PRIVMSG #" + self.CHANNEL + " :" + message
+		encoded_send(self.socket, messageTemp+"\r\n")
+		print("SENT: " + messageTemp)
 
+def encoded_send(socket, msg:str):
+	socket.send(msg.encode("utf-8"))
+
+if __name__ == "__main__": 
+	a = tc_bot("twitch_tries","bad_broker_bot","oauth:8298y40ehg7i3drbqjggqi49byztry")
+	a.sendMessage("HELLO HELLO")
 
 
